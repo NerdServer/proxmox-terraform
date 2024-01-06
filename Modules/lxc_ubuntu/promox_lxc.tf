fd920c460c5e7914fcbdf5451c0620469ba2b154
Tannerru <tanner.rusher@outlook.com>
2024-01-06 00:40:55 -0600

    3

--- Modules/lxc_ubuntu/promox_lxc.tf
+++ Modules/lxc_ubuntu/promox_lxc.tf
@@ -14,7 +14,7 @@ resource "proxmox_lxc" "lxc_containers" {
   for_each     = var.lxc_containers
   target_node  = each.value.target_node
   hostname     = local.container_names[tostring(each.key)]
-  ostemplate   = "ubuntu22-04-template"
+  ostemplate   = "ISO:vztmpl/ubuntu-20.04-standard_20.04-1_amd64.tar.gz"
   ssh_public_keys = var.ssh_public_keys
   
   rootfs {
