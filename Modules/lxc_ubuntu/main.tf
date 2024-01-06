cec5dd36447a81ebfd07f2b4f1dbb7e22126cdfb
Tannerru <tanner.rusher@outlook.com>
2024-01-06 13:39:03 -0600

    testing with plaintext, will revoke key

--- Modules/lxc_ubuntu/main.tf
+++ Modules/lxc_ubuntu/main.tf
@@ -1,19 +1,21 @@
 terraform {
 
-backend "s3" {
-    bucket = "terraform-tstates"
-    key = "./lxc_ubuntu/terraform.tfstate"
-    region = "main"        
-    skip_credentials_validation = true
-    skip_requesting_account_id = true
-    skip_metadata_api_check = true
-    skip_region_validation = true
-    skip_s3_checksum = true
-    use_path_style = true
+  backend "s3" { 
+    bucket = "terraform.tfstates" 
+    region = "main" 
+    key = "tf.tfstate" 
+    skip_region_validation = true 
+    skip_credentials_validation = true 
+    skip_requesting_account_id = true 
+    use_path_style = true 
     insecure = true 
-  }
-
-
+    skip_metadata_api_check = true 
+    skip_s3_checksum = true 
+    access_key = "4c0rKemwArm75NWThpCI" 
+    secret_key = "iHhu7JenbY9OML4xBcJp6EVrVuzAfOgVEC1b1a5w" 
+    endpoints = { s3 = "http://10.0.0.243:9002" } 
+    } 
+    
   required_providers {
     proxmox = {
       source  = "TheGameProfi/proxmox"
