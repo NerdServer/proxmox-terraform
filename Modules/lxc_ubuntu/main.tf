3b591b1c4e23be4c267590cda971f7ff8aba9073
Tannerru <tanner.rusher@outlook.com>
2024-01-05 23:30:14 -0600

    5

--- Modules/lxc_ubuntu/main.tf
+++ Modules/lxc_ubuntu/main.tf
@@ -3,10 +3,14 @@ terraform {
     proxmox = {
       source  = "TheGameProfi/proxmox"
       version = "2.9.16"
-      proxmox_api_url= "${var.proxmox_api_url}"
-      proxmox_proxmox_api_token_id= "${var.proxmox_api_token_id:}"
-      proxmox_api_token_secret= "${var.proxmox_api_token_secret}"
+      
 
     }
   }
+}
+provider "proxmox" {
+    proxmox_api_url= "${var.proxmox_api_url}"
+    proxmox_proxmox_api_token_id= "${var.proxmox_api_token_id}"
+    proxmox_api_token_secret= "${var.proxmox_api_token_secret}"
+
 }
