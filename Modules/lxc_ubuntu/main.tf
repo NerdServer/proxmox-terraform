9a7bb5f1a0bd96a7b0a2155a2cab57415331e881
Tannerru <tanner.rusher@outlook.com>
2024-01-05 23:32:44 -0600

    6

--- Modules/lxc_ubuntu/main.tf
+++ Modules/lxc_ubuntu/main.tf
@@ -8,9 +8,13 @@ terraform {
     }
   }
 }
-provider "proxmox" {
-    proxmox_api_url= "${var.proxmox_api_url}"
-    proxmox_proxmox_api_token_id= "${var.proxmox_api_token_id}"
-    proxmox_api_token_secret= "${var.proxmox_api_token_secret}"
 
+provider "proxmox" {
+
+ pm_api_url = "${var.proxmox_api_url}"
+ pm_api_token_id = "${var.proxmox_api_token_id}"
+ pm_api_token_secret = "${var.proxmox_api_token_secret}"
+
+# (Optional) Skip TLS Verification
+ pm_tls_insecure = true
 }
