4f37378f6b015661299f89b838570d3837a9ff10
Tannerru <tanner.rusher@outlook.com>
2024-01-06 12:35:47 -0600

    1

--- Modules/lxc_ubuntu/main.tf
+++ Modules/lxc_ubuntu/main.tf
@@ -1,8 +1,14 @@
 terraform {
 
 backend "s3" {
-        config_file = "backend.tfvars"
-   }
+    bucket = "terraform-tstates"
+    key = "terraform.tfstate"
+    region = "main"
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    skip_region_validation = true
+    force_path_style = true
+  }
 
 
   required_providers {
