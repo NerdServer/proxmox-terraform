7010de12fa6e37b54c633e292f55a3761d3769b6
Tannerru <tanner.rusher@outlook.com>
2024-01-05 22:57:16 -0600

    formatting

--- Modules/lxc_ubuntu/main.tf
+++ Modules/lxc_ubuntu/main.tf
@@ -1,17 +1,8 @@
-# Proxmox Provider
-# ---
-# Initial Provider Configuration for Proxmox
-
 terraform {
- required_providers {
+  required_providers {
     proxmox = {
       source  = "TheGameProfi/proxmox"
       version = "2.9.15"
     }
   }
-}
-
-provider "proxmox" {
-  # (Optional) Skip TLS Verification
-  pm_tls_insecure = true
 }
---
+++ multi-lxc/.terragrunt-cache/c3h2g_UXmMoU-lAG9eppqKYm2fY/F33LGtg1_vjhU1RAvfr57wyQhFE/terragrunt.hcl
@@ -0,0 +1,17 @@
+terraform {
+  source = "../modules/lxc_ubuntu"
+}
+
+locals {
+  containers = { for i in range(1, 2) : i => {
+    target_node    = "nerd-pve02"
+    rootfs_storage = "ssd1"
+    rootfs_size    = "8G"
+    tags           = "lxc"
+
+  } }
+}
+
+inputs = {
+  lxc_containers = local.containers
+}
