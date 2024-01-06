a7e4b52cac36070214079312859733f38923c2f5
Tannerru <tanner.rusher@outlook.com>
2024-01-05 22:39:11 -0600

    formatting

--- Modules/lxc_ubuntu/main.tf
+++ Modules/lxc_ubuntu/main.tf
@@ -2,23 +2,22 @@
 # ---
 # Initial Provider Configuration for Proxmox
 
-  
-
 terraform {
-backend "git" {
-    path       = "lxc-multi/state/terraform.tfstate"
+  backend "git" {
+    path = "lxc-multi/state/terraform.tfstate"
+  }
 
-required_version = ">= 0.13.0"
-}
-required_providers {
-proxmox = {
-source = "TheGameProfi/proxmox"
-version = "2.9.15"
-}
-}
+  required_version = ">= 0.13.0"
+  
+  required_providers {
+    proxmox = {
+      source  = "TheGameProfi/proxmox"
+      version = "2.9.15"
+    }
+  }
 }
 
 provider "proxmox" {
-# (Optional) Skip TLS Verification
- pm_tls_insecure = true
+  # (Optional) Skip TLS Verification
+  pm_tls_insecure = true
 }
