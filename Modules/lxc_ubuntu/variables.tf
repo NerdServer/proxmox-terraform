c2741068935c0e7b258d23bbea4e9b40e700ae95
Tannerru <tanner.rusher@outlook.com>
2024-01-06 00:14:54 -0600

    7

--- Modules/lxc_ubuntu/variables.tf
+++ Modules/lxc_ubuntu/variables.tf
@@ -1,8 +1,6 @@
 variable "lxc_containers" {
   type = map(object({
     target_node     = string
-    ostemplate      = string
-    ssh_public_keys = string
     rootfs_storage  = string
     rootfs_size     = string
   }))
